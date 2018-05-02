1dd2e9488ef45104e7259d527bc35109ef04ce25
Pawel Klasa <pawel.klasa@gmail.com>
2018-05-02 17:11:51 +0100

    update

---
+++ lecture-01/exercise-01/app.js
@@ -0,0 +1,16 @@
+var names = ['John', 'Ruth', 'Peter', 
+{
+  name: 'Pav',
+  surname: 'Klasa'
+},
+{
+  something: 3
+}
+];
+
+var datesOfBirth = [23, 35, 40];
+
+for (var i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
