35514c5571c878768cf59eb419da549ba6c910dc
Pawel Klasa <pawel.klasa@gmail.com>
2018-05-08 10:26:11 +0100

    largest smallest

--- lecture-01/exercise-01/app.js
+++ lecture-01/exercise-01/app.js
@@ -6,3 +6,25 @@ for (var i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
 
+
+
+numbers = [2, 4, 9, 2, 0, 16, 24]
+
+var largest = numbers[0];
+var smallest = numbers[0];
+
+for (var i = 1; i < numbers.length; i++) {
+
+    if (numbers[i] > largest) {
+        largest = numbers[i];
+    }
+    else if(numbers[i] < smallest) {
+        smallest = numbers[i];
+    }
+
+    
+
+}
+
+console.log(largest);
+    console.log(smallest);
