10b8c71d18f09807b21b0e13d3b7cd51070978ca
Pawel <pawel.klasa@gmail.com>
2018-05-08 21:16:26 +0100

    created person constructor

--- lecture-02/exercise-02/app.js
+++ lecture-02/exercise-02/app.js
@@ -5,30 +5,40 @@
 
 // Step 1: 
 // First object is john 
-let john = {
-  age: 20,
-  name: 'John:'
-};
+
 // Step 2: 
 // Second object is Ruth 
-let ruth = {
-  age: 30,
-  name: 'Ruth:'
-};
+
 // Step 3: 
 // Third object is Peter 
-let peter = {
-  age: 40,
-  name: 'Peter:'
-};
+
 // Step 4: 
 // Add those three objects to an array that you have to create 
-let newArray = [john, ruth, peter];
+
 // Step 5: 
 // Using the filter method get the younger and older object 
-let result = newArray.filter(function() {
-  
-});
+
 // Step 6: 
 // print the values here
-console.log(result);
+
+// Creating constructor
+function Person(age, name) {
+  this.age = age;
+  this.name = name;
+}
+
+let john = new Person(22, 'John');
+let ruth = new Person(25, 'Ruth');
+let peter = new Person(30, 'Peter');
+
+Person.prototype.doSomething = function() {
+  return `Welcome ${this.name} you are ${this.age} years old`;
+}
+
+let newArray = [john, ruth, peter];
+
+
+
+console.log(john.doSomething());
+
+console.log(newArray);
